c8e6b8b707b4c8bcb16f7e789fe280dd520e79c2
imhim395 <yuvaangulati7689@gmail.com>
2026-03-22 13:32:59 -0400

    Initial Commit

---
+++ .index.py
@@ -0,0 +1,5 @@
+metals = ["copper", "gold", "silver", "iron"]
+print(metals.index("silver"))#gives index number of silver
+
+metals = ["copper", "gold", "silver", "iron"]
+print(metals.index("diamond"))#error because there is no diamond in the list
