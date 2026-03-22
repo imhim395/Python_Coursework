c8060b67399c235dc9d8345df860f05734cfb46e
imhim395 <yuvaangulati7689@gmail.com>
2026-03-22 10:57:01 -0400

    Initial Commit

---
+++ indexes and list slicing exercises.py
@@ -0,0 +1,7 @@
+li = [[0,2],[4,6],[8,10],[12,14]]
+print(li[0])
+print(li[3][1])
+
+li2 = ["chairs", "table", "desk", "lamp", "bed"]
+print(li2[-5])
+print("most people own at least", li[0][1], li2[0])
