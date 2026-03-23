217a38e6b534f52f4c602cb99217eb8f6bd32613
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 10:05:30 -0400

    Initial Commit

---
+++ lists vs strings.py
@@ -0,0 +1,7 @@
+ex = [1,2,3]#lists are mutable which means they can be changed
+ex[0] = 0#changes index 0 which is 1 to 0
+print(ex)
+#strings are immutable which means they cant be changed
+ex2 = "6741"
+ex2 = "4167"#u can reassign the value tho
+print(ex2)
