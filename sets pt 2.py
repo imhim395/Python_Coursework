0a1bd88d6ec45ee04bd0d040b4efd46d226cd05f
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 14:25:59 -0400

    Initial Commit

---
+++ sets pt 2.py
@@ -0,0 +1,7 @@
+set = {3,6,9,12,15}#u cant use indexes to access individual pieces of data
+
+for num in set:
+    print(num)
+
+print(12 in set)#true cause 12 is in the set
+print("hi" in set)#false cause hi isnt in the set
