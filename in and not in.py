e9b3e1859716ab758a947c0e16133a2eab972acf
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 10:40:54 -0400

    Initial Commit

---
+++ in and not in.py
@@ -0,0 +1,8 @@
+first = {0: 2.1, 1: 2.2, 2: 2.3, 3: 2.4}
+print(0 in first)#true cause 0 is in the dictionary first
+
+first = {0: 2.1, 1: 2.2, 2: 2.3, 3: 2.4}
+print(5 in first)#false cause 5 isnt in the dictionary
+
+first = {0: 2.1, 1: 2.2, 2: 2.3, 3: 2.4}
+print(5 not in first)#true cause 5 is not in the dictionary
