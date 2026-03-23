f85f6d04c733a871e479c6b942b514bd83c418c3
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 10:58:39 -0400

    Initial Commit

---
+++ .keys().py
@@ -0,0 +1,5 @@
+birth_years = {1994: "bill", 1969: "emily", 1982: "elizabeth", 2000: "turner"}
+print(birth_years.keys())#gives all of the keys in a dictionary
+
+for key in birth_years:
+    print(key)
