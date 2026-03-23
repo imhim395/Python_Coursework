c20be4fbd9ad1bd853f1b2a4dbc2780bbadfc3d7
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 10:50:04 -0400

    Initial Commit

---
+++ introduction to dictionaries exercises.py
@@ -0,0 +1,4 @@
+dictionar = {"thdx": 1, "floats": 0.5, "rapper": "liltecca", "mathcomp": "amc 10", "sciencecomp": "scibowl"}
+print(dictionar["rapper"])
+print("floats" in dictionar)
+print("u know" not in dictionar)
