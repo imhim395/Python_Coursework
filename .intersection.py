14aa4dc4e794c2b5bb901affe20afbf2b8d8624b
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 14:39:15 -0400

    Initial Commit

---
+++ .intersection.py
@@ -0,0 +1,5 @@
+#this finds what is in common between two sets
+no = {1,2,3,4,5,6}
+yes = {0,1,2,3,4,5,6,7}
+inter = no.intersection(yes)#finds the intersection of no and yes
+print(inter)#prints out what is in common between two sets
