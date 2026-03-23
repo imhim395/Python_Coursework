7c380c6936b9444395298452a7620c8d38c84034
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 12:08:18 -0400

    Initial Commit

---
+++ .update().py
@@ -0,0 +1,5 @@
+city_info = {"country": "Canada", "province": "Ontario", "city": "Toronto"}
+population = {"population": 2930000}
+city_info.update(population)#updates the dictionary with population key value pair from the population variable
+print(city_info)
+print(population)#this hasnt been changed
