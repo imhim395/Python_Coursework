9c85063751b861e2112bb4fdfd1b3f23e69a92c5
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 10:37:47 -0400

    Initial Commit

---
+++ dictionary equivilancy.py
@@ -0,0 +1,3 @@
+first = {0: 2.1, 1: 2.2, 2: 2.3, 3: 2.4}
+second = {2: 2.3, 0: 2.1, 3: 2.4, 1: 2.2}
+print(first == second)#true because they dont have to be in the same order, they js have to have the same values
