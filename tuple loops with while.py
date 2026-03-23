85ac7b712a40ce75759279ccd0a170f3f879faff
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 14:04:23 -0400

    Initial Commit

---
+++ tuple loops with while.py
@@ -0,0 +1,14 @@
+major_cities = ("Tokyo", "London", "New York", "Shanghai", "Sydney")
+
+count = 0
+while count < len(major_cities):
+    print(major_cities[count])
+    count+=1
+
+major_cities = ("Tokyo", "London", "New York", "Shanghai", "Sydney")
+
+backwards = len(major_cities)-1
+while backwards >=0:
+    print(major_cities[backwards])
+    backwards-=1#I DONT LIKE WHILE LOOPS
+
---
+++ tuple loops.py
@@ -0,0 +1,4 @@
+major_cities = ("Tokyo", "London", "New York", "Shanghai", "Sydney")
+
+for cities in major_cities:
+    print(cities)
