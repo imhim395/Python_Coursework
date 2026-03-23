ecbc98cb8fe6d376e475f3e531ce4dc96d3e78ec
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 14:32:11 -0400

    Initial Commit

---
+++ .discard().py
@@ -0,0 +1,3 @@
+fruits = {"apple", "orange", "banana", "tomato"}
+fruits.discard("pear")#no error message because that is the only difference from discard and remove
+print(fruits)
---
+++ .remove().py
@@ -0,0 +1,3 @@
+fruits = {"apple", "orange", "banana", "tomato"}
+fruits.remove("tomato")#removes tomato from the set
+print(fruits)
