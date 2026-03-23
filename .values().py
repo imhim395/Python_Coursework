dcc687d84b5dec3ae8ec5b214ace98d99b47c09a
imhim395 <yuvaangulati7689@gmail.com>
2026-03-23 10:59:32 -0400

    Initial Commit

---
+++ .values().py
@@ -0,0 +1,5 @@
+birth_years = {1994: "bill", 1969: "emily", 1982: "elizabeth", 2000: "turner"}
+print(birth_years.values())#gives values in the dictionary
+
+for value in birth_years:
+    print(value)
